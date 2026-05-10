c3aea184822ec631d6ccc26b08d4aa82da1c60c5
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-05-10 14:26:27 +0300

    use bloc provider avec material app inside main. dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:basketball_points/cubits/counter_cubit.dart';
 import 'package:basketball_points/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(BasketballPoints());
@@ -8,6 +10,9 @@ void main() {
 class BasketballPoints extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()),
+    );
   }
 }
