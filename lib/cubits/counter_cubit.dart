9d9ff90f0adc86019bb795a58887ee6d3de69b2f
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-05-10 14:09:25 +0300

    logic of counter cubit

--- lib/cubits/counter_cubit.dart
+++ lib/cubits/counter_cubit.dart
@@ -2,5 +2,19 @@ import 'package:basketball_points/cubits/counter_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit(super.initialState);
+  CounterCubit() : super(CounterAIncrementState());
+
+  int teamAPoints = 0;
+
+  int teamBPoints = 0;
+
+  void TeamAIncrement({required String team, required int buttonNumber}) {
+    if (team == "A") {
+      teamAPoints += buttonNumber;
+      emit(CounterAIncrementState());
+    } else {
+      teamBPoints += buttonNumber;
+      emit(CounterBIncrementState());
+    }
+  }
 }
